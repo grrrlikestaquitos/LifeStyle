7b439de38344becc5b42092528d11a142409a391
Andrei Villasana <Andrei@Andreis-MacBook-Pro.local>
2017-05-19 16:09:41 -0700

    algorithm to remove beacon if it is no longer within proximity.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log('Beacon List was updated: '+nextProps.app.beaconList);
+        // console.log('Beacon List was updated: '+nextProps.app.beaconList);
     }
 
     checkNativeArray(event) {
@@ -46,37 +46,39 @@ class App extends Component {
         const { beaconList } = app;
         console.log('Check native array method: '+event);
         console.log('Props array: '+beaconList);
-
-        if ((event.length > 0) && (beaconList.length > 0)) {
-            if (event.length > beaconList.length) {
-                //A new beacon(s) was introduced - scenario
-                //if nativeEvent has more than beaconList
-                const difference = (event.length-beaconList.length);
-                console.log('Event array has '+(difference)+' more element(s) than beacon List');
-                for (i = beaconList.length; i < event.length; i++) {
-                    console.log('Index of: '+i);
+        
+        if (event.length > 0) {
+            if ((event.length > 0) && (beaconList.length > 0)) {
+                if (event.length > beaconList.length) {
+                    //A new beacon(s) was introduced - scenario
+                    //if nativeEvent has more than beaconList
+                    const difference = (event.length-beaconList.length);
+                    console.log('Event array has '+(difference)+' more element(s) than beacon List');
+                    for (i = beaconList.length; i < event.length; i++) {
+                        actions.addNewBeacon(event[i]);
+                    }
+                } else if (event.length < beaconList.length) {
+                    //An existing beacon has been removed
+                    for (i = 0; i < beaconList.length; i++) {
+                        const beacon = beaconList[i];
+                        if (!(event.includes(beacon))) {
+                            console.log('Removing beacon: '+beacon);
+                            actions.removeBeacon(beacon);
+                        }
+                    }
+                }
+            } else {
+                //First time sighted a beacon
+                for (i = 0; i < event.length; i++) {
+                    console.log(`Added beacon ${event[i]} into the beaconList array`);
                     actions.addNewBeacon(event[i]);
                 }
-            } else if (event.length < beaconList.length) {
-                //An existing beacon has been removed
             }
-            // for (i = 0; i < event.length; i++) {
-            //     for (x = 0; x < beaconList.length; x++) {
-            //         if (event[i] === beaconList[x]) {
-            //             //this check if items in both arrays are the same, if so, do nothing
-            //             console.log(`${event[i]} and ${beaconList[x]} are a match!`)
-            //         } else if (event[i] !== beaconList[x]) {
-            //             //event array is not in beaconList array
-            //             //therefore array would have to append the new value
-            //             console.log(event[i]+' is not in beaconList array');
-            //         }
-            //     }
-            // }
-        } else {
-            //First time sighted a beacon
-            for (i = 0; i < event.length; i++) {
-                console.log(`Added beacon ${event[i]} into the beaconList array`);
-                actions.addNewBeacon(event[i]);
+        } else if (event.length === 0) {
+            console.log('Event is empty');
+            for (i = 0; i < beaconList.length; i++) {
+                const beacon = beaconList[i];
+                actions.removeBeacon(beacon);
             }
         }
 
@@ -90,9 +92,7 @@ class App extends Component {
                 if (error) {
                     console.error(error);
                 } else {
-                    if (event.length > 0) {
-                        this.checkNativeArray(event);
-                    }
+                    this.checkNativeArray(event);
                 }
             }),
             10000
@@ -104,7 +104,6 @@ class App extends Component {
     }
 
     render() {
-        console.log('App is being rendered, contents in beaconList: ' + this.props.app.beaconList);
         return(
             <View style={{ flex: 1, marginTop: 20 }}>
                 <AnimatedHearts />
