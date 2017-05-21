4eda1ba76c7a611152898842ff239b760a526fcb
Andrei Villasana <Andrei@Andreis-MacBook-Pro.local>
2017-05-20 21:37:24 -0700

    implemented new values into constants

--- src/app/constants.js
+++ src/app/constants.js
@@ -4,3 +4,19 @@ export const COLORS = {
     heartRed: 'rgba(250, 130, 138, .75)',
     transparent: 'transparent'
 };
+
+export const BEACON_LOC_ID = {
+    bathroom: '0117C53D1EEA',
+    bedroom: '0117C5350AFA',
+    office: '0117C533D7D0'
+};
+
+export const INSPIRATIONAL_QUOTES = {
+    welcome: [
+        'There are two great days in a person\'s life - the day we are born and the day we discover why.',
+        'Put your heart, mind, and soul into even your smallest acts. This is the secret of success.',
+        'Happiness is not something you postpone for the future; it is something you design for the present.',
+        'Believe you can and you\'re halfway there.',
+        'As we express our gratitude, we must never forget that the highest appreciation is not to utter words, but to live by them.'
+    ]
+}
--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,14 @@ import AntroText from './components/AntroText';
 import AnimatedHearts from './components/FloatingHearts';
 import * as actions from './actions';
 import reducer from './reducer';
-import { COLORS } from './constants';
+import { COLORS, BEACON_LOC_ID, INSPIRATIONAL_QUOTES } from './constants';
 
 export default {
     AntroText,
     AnimatedHearts,
     actions,
     reducer,
-    COLORS
+    COLORS,
+    BEACON_LOC_ID,
+    INSPIRATIONAL_QUOTES
 };
